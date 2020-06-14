00a42173ac7adad8f229b5c1764d56fc4ad73839
supun desktop <supun.chamikara@rocketmail.com>
2020-06-14 08:07:42 +0530

    Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// @route       get api/profile
-// @desc        test route
-// @accress     public
+// @route       get api/profile/me
+// @desc        get current users profile
+// @accress     privete
 
-router.get("/", (req, res) => res.send("Profile route"));
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await (
+      await Profile.findOne({ user: req.user.id })
+    ).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+});
 
 module.exports = router;
