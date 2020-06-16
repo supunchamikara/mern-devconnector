486a44b02a4a7299e82e3dbe0fbdab0a8b4c98bd
supun desktop <supun.chamikara@rocketmail.com>
2020-06-16 16:12:24 +0530

    creating the post model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,9 @@ const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
+const request = require("request");
+const config = require("config");
+
 const { check, validationResult } = require("express-validator");
 
 // @route       get api/profile/me
@@ -302,6 +305,35 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Sever error");
+  }
+});
+
+// @route       get api/profile/github/:username
+// @desc        get user repos from github
+// @accress     public.
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      methode: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No git hub profile found!" });
+      }
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server err");
   }
 });
 
